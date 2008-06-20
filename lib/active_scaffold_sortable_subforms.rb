7abe05b78507294d933706f649ef9a1ae6f6b915
Andrew Roth <andrewroth@andrew-roths-macbook-pro.local>
2008-06-20 04:52:05 -0700

    fixed bug with hard-coding position instead of using the subform_column, thanks to kbon on the AS mailing list (http://groups.google.com/group/activescaffold/browse_thread/thread/826471e22720d939/63d5f53a5997ebc6?lnk=gst&q=kbon#63d5f53a5997ebc6)

--- lib/active_scaffold_sortable_subforms.rb
+++ lib/active_scaffold_sortable_subforms.rb
@@ -7,7 +7,7 @@ module ActiveScaffoldSortableSubforms
     def active_scaffold_sortable_subform(subsorts)
       subsorts.each_pair do |k,v|
         define_method("#{k.to_s.singularize}_#{v}_form_column") do |record, name|
-          text_field_tag(name, record.position, :class => 'association-position hidden', :style => 'display:none') + 
+          text_field_tag(name, record.send(v), :class => 'association-position hidden', :style => 'display:none') + 
             content_tag(:div, image_tag("arrow_move.png"), :class => "mover") +  
             "<script>as_dd_reorder_setup($('#{name}'));</script>"
         end
